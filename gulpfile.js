0f7a417f5dc05615a7b00d59d16a41a807e267a4
Єлизавета Руда <lizarudayaa@gmail.com>
2021-05-21 01:18:16 +0300

    commit

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const { src, dest, series, parallel, watch } = require('gulp')
+const browserSync = require('browser-sync').create()
+const origin = 'build'
+const destination = 'build'
+
+function html(cb) {
+  cb()
+}
+
+function css(cb) {
+  cb()
+}
+
+function js(cb) {
+  cb()
+}
+
+function watcher(cb) {
+  watch(`${origin}/**/*.html`).on('change', series(html, browserSync.reload))
+  watch(`${origin}/**/*.css`).on('change', series(css, browserSync.reload))
+  watch(`${origin}/**/*.js`).on('change', series(js, browserSync.reload))
+  cb()
+}
+
+function server(cb) {
+  browserSync.init({
+    notify: false,
+    open: false,
+    server: {
+      baseDir: destination
+    }
+  })
+  cb()
+}
+
+exports.default = series(parallel(html, css, js), server, watcher)
+
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./build/**/*")
+    .pipe(deploy())
+});
